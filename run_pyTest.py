58d37c4578a8f1947a4f5c67ddf5c62ba43259d5
amnpt <amnpt123@163.com>
2022-03-31 16:08:17 +0800

    Signed-off-by: amnpt <amnpt123@163.com>

--- run_pyTest.py
+++ run_pyTest.py
@@ -7,7 +7,7 @@
 import pytest
 import unittest
 import time
-# from common.Email import send_email
+from common.Email import send_email
 import cx_Oracle
 
 
@@ -16,6 +16,6 @@ import cx_Oracle
 
 if __name__ == "__main__":
     #report = 'G:/AutoTest/EpspAutoTest/report/report_' + time.strftime("%Y%m%d%H%M%S",time.localtime(time.time())) + '.html'
-    pytest.main(["-s", "G:/Git/pytest_testApi/testCase/apiTest", "--pytest_report", report])
     #pytest.main(["-s", "G:/AutoTest/EpspAutoTest/testCase/apiTest/test_nativePay.py", "--pytest_report",'G:/AutoTest/EpspAutoTest/report/report_' + time.strftime("%Y%m%d%H%M%S",time.localtime(time.time())) + '.html'])
-    #send_email(report)   #163邮箱，需要开启 IMAP/SMTP服务
+    pytest.main(["-s", "G:/AutoTest/EpspAutoTest/testCase/apiTest", "--pytest_report",'G:/AutoTest/EpspAutoTest/report/report_' + time.strftime("%Y%m%d%H%M%S",time.localtime(time.time())) + '.html'])
+  #  send_email(report)   #163邮箱，需要开启 IMAP/SMTP服务
